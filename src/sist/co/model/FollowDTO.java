2d7bc459a1d06dff77a55cb275296efcc8400c7e
sist\John <gowhql@naver.com>
2016-09-12 11:15:44 +0900

    PEED DB에서 가져오는 중....

--- src/sist/co/model/FollowDTO.java
+++ src/sist/co/model/FollowDTO.java
@@ -29,16 +29,18 @@ public class FollowDTO implements Serializable{
 	private int seq;
 	private int member_seq;
 	private int follow;
+	private int follow_connect;
 	
 	public FollowDTO() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public FollowDTO(int seq, int member_seq, int follow) {
+	public FollowDTO(int seq, int member_seq, int follow, int follow_connect) {
 		super();
 		this.seq = seq;
 		this.member_seq = member_seq;
 		this.follow = follow;
+		this.follow_connect = follow_connect;
 	}
 
 	public int getSeq() {
@@ -65,11 +67,20 @@ public class FollowDTO implements Serializable{
 		this.follow = follow;
 	}
 
+	public int getFollow_connect() {
+		return follow_connect;
+	}
+
+	public void setFollow_connect(int follow_connect) {
+		this.follow_connect = follow_connect;
+	}
+
 	@Override
 	public String toString() {
-		return "FollowDTO [seq=" + seq + ", member_seq=" + member_seq + ", follow=" + follow + "]";
+		return "FollowDTO [seq=" + seq + ", member_seq=" + member_seq + ", follow=" + follow + ", follow_connect="
+				+ follow_connect + "]";
 	}
-	
+
 	
 	
 }
